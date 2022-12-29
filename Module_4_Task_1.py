5cecd075f056bd566b613270e01f5a8d195b6bd7
natalkachernova <120986642+natalkachernova@users.noreply.github.com>
2022-12-29 22:28:51 +0200

    Add files via upload

--- Module_4_Task_1.py
+++ Module_4_Task_1.py
@@ -1,9 +1,18 @@
-def check_palindrom(word):
+def check_palindrome(word):
+
+    """ 
+    This function checks if the given word is a palindrome or not 
+    :param: string 
+    :return: boolean 
+    """
+
     new_word = word[::-1]
     if new_word == word:
         return True
     
-if check_palindrom(input ("Insert a word: ")) == True:
-    print("Palindrom")
+if check_palindrome(input ("Insert a word: ")):
+    print("Palindrome")
 else:
-    print("No Palindrom")
+    print("No Palindrome")
+
+#help(check_palindrome)
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# CheckPalindrom
+# CheckPalindrome
+
+Help on function check_palindrome in module __main__:
+
+check_palindrome(word)
+    This function checks if the given word is a palindrome or not 
+    :param: string 
+    :return: boolean
